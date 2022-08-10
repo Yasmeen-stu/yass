7531cdd71b1ae774bd28122ba609c1c34e092c43
Yasmeen-stu <110789894+Yasmeen-stu@users.noreply.github.com>
2022-08-10 23:30:55 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.thedessert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    String dessertname[] = {"Molten Cake", "Chocolate Cake", "Travel Cake", "Red Velvet Cake",
+            "Muffin Cake", "Cup Cake", "Cinabon", "Coffee Dessert", "Cookies",
+            "Creme Caramel", "Donuts", "Eklerek", "Macaron", "Mille Feuille",
+            "Waffle"};
+    String dessertprice[] = {"Price:50LE", "Price:40LE", "Price:70LE", "Price:55LE", "Price:60LE", "Price:30LE",
+            "Price:20LE", "Price:25LE", "Price:25LE", "Price:35LE", "Price:30LE", "Price:25LE", "Price:80LE", "Price:65LE", "Price:45LE"};
+    int dessertimage[] = {R.drawable.moltencake, R.drawable.chocolatecake, R.drawable.travelcake, R.drawable.redvelvetcake
+            , R.drawable.muffincake, R.drawable.cupcake, R.drawable.cinabon, R.drawable.coffeedessert
+            , R.drawable.cookies, R.drawable.cremecaramel, R.drawable.donuts, R.drawable.eklerek,
+            R.drawable.macaron, R.drawable.millefeuille, R.drawable.waffle};}
