1048639b52567c87038584ea3c6f7b82f4ab65ab
Yasmeen-stu <110789894+Yasmeen-stu@users.noreply.github.com>
2022-08-10 22:22:18 +0200

    Update food.java

--- app/src/main/java/com/example/resturant/food.java
+++ app/src/main/java/com/example/resturant/food.java
@@ -28,3 +28,29 @@ public class food {
 }
 
 
+public class Dessert {
+    private int image;
+    private String textd;
+
+    public Dessert(int image, String textd) {
+        this.image= image;
+        this.textd = textd;
+    }
+
+    public int getImg() {
+        return image;
+    }
+
+    public void setImg(int iamge) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return textd;
+    }
+
+    public void setText(String text) {
+        this.textd= textd;
+    }
+}
+
